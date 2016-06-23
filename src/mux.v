319923ca15578a5c86ddf019048680c5bfb1b0a4
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2016-06-23 18:44:54 -0300

    Mux working.

--- src/mux.v
+++ src/mux.v
@@ -1,38 +1,37 @@
 module mux
 ( input  [2:0] regSelect,
-  input  [8*15:0] regBank,
-  input  rSelect,
-  input [15:0] regR,
+  input  regRSelect,
   input  immSelect,
   input [15:0] imm,
+  input [15:0] regR,
+  input [15:0] reg0,
+  input [15:0] reg1,
+  input [15:0] reg2,
+  input [15:0] reg3,
+  input [15:0] reg4,
+  input [15:0] reg5,
+  input [15:0] reg6,
+  input [15:0] reg7,
   output [15:0] outData);
 
-reg [15:0] regBank_array [2:0];
 reg [15:0] regOut;
-
-genvar i;
-generate for(i = 0; i < 16; i = i +1)
-  begin
-    assign regBank[16*i+:16] = regBank_array[i];
-  end
-endgenerate
 
 assign outData = regOut;
 
 always @(*) begin
   // Retrieve output from registers bank
   case(regSelect)
-    3'b000: regOut <= regBank_array[3'b000];
-    3'b001: regOut <= regBank_array[3'b001];
-    3'b010: regOut <= regBank_array[3'b010];
-    3'b011: regOut <= regBank_array[3'b011];
-    3'b100: regOut <= regBank_array[3'b100];
-    3'b101: regOut <= regBank_array[3'b101];
-    3'b110: regOut <= regBank_array[3'b110];
-    3'b111: regOut <= regBank_array[3'b111];
+    3'b000: regOut <= reg0;
+    3'b001: regOut <= reg1;
+    3'b010: regOut <= reg2;
+    3'b011: regOut <= reg3;
+    3'b100: regOut <= reg4;
+    3'b101: regOut <= reg5;
+    3'b110: regOut <= reg6;
+    3'b111: regOut <= reg7;
   endcase
   // Retrieve output from R register
-  if(rSelect == 1'b1)
+  if(regRSelect == 1'b1)
     regOut <= regR;
   // Retrieve output from immediate
   if(immSelect == 1'b1)
--- testbench/mux_testbench.v
+++ testbench/mux_testbench.v
@@ -2,12 +2,19 @@
 
 module mux_testbench;
 
-reg [2:0]  regSelect;
-reg [15:0] regBank [2:0];
-reg rSelect;
+reg regRSelect;
 reg [15:0] regR;
 reg immSelect;
 reg [15:0] imm;
+reg [2:0]  regSelect;
+reg [15:0] reg0;
+reg [15:0] reg1;
+reg [15:0] reg2;
+reg [15:0] reg3;
+reg [15:0] reg4;
+reg [15:0] reg5;
+reg [15:0] reg6;
+reg [15:0] reg7;
 wire [15:0] outData;
 
 initial $dumpfile("mux_testbench.vcd");
@@ -15,9 +22,17 @@ initial $dumpvars(0, mux_testbench);
 
 initial
 begin
+  #1 reg0 = 0; reg1 = 0; reg2 = 3; reg4 = 5;
+  reg5 = 6; reg6 = 15; reg7 = 10; regR = 30;
+  imm = 42; regSelect = 2; immSelect = 0; regRSelect = 0;
+  #1 regRSelect = 1;
+  #1 regRSelect = 0; immSelect = 1;
+  #1 immSelect = 0; regSelect = 6;
+  #1 regSelect = 3; reg3 = 8;
   #5 $finish;
 end
 
-mux m(regSelect, regBank, rSelect, regR, immSelect, imm, outData);
+mux m(regSelect, regRSelect, immSelect, imm, regR, reg0,
+reg1, reg2, reg3, reg4, reg5, reg6, reg7, outData);
 
 endmodule
